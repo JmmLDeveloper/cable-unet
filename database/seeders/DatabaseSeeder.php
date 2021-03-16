d61ff0aa85036db1e3c1641d5f3c382136a74136
Jose Marquez <josemmarquezl@gmail.com>
2021-03-16 11:16:26 -0400

    add : users in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Database\Seeders\ChannelSeeder;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Log;
@@ -10,9 +11,24 @@ class DatabaseSeeder extends Seeder
 {
 
 
-
     public function run()
     {
+        $userA = new User;
+        $userA->name = 'jose miguel';
+        $userA->email = 'admin@gmail.com';
+        $userA->password = '12345678';
+        $userA->is_admin = 1;
+
+        $userB = new User;
+        $userB->name = 'pedro perez';
+        $userB->email = 'sub@gmail.com';
+        $userB->password = '12345678';
+        $userB->is_admin = 0;
+
+        $userA->save();
+        $userB->save();
+
+
         $this->call([
             ChannelSeeder::class,
         ]);
